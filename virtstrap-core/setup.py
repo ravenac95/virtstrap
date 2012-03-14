ffa0087bfdac7e2e6c5a15b741683ac667d9ab9a
Reuven V. Gonzales <reuven@tobetter.us>
2012-03-14 11:38:14 -0700

    Updated to 0.3.3-dev

--- virtstrap-core/setup.py
+++ virtstrap-core/setup.py
@@ -8,7 +8,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.3.2"
+VERSION = "0.3.3-dev"
 
 # Installation requirements
 REQUIREMENTS = [
--- virtstrap/setup.py
+++ virtstrap/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-VERSION = '0.3.2'
+VERSION = '0.3.3-dev'
 
 LONG_DESCRIPTION = open('README.rst').read()
 
