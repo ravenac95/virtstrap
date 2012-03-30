1d4e95f781a26fc25dbf3857d1826ded0a470969
Reuven V. Gonzales <reuven@tobetter.us>
2012-03-29 17:56:12 -0700

    Adding loaders and environment for tempita

--- virtstrap-core/tests/test_templating.py
+++ virtstrap-core/tests/test_templating.py
@@ -2,8 +2,10 @@
 Test Templating
 ---------------
 """
+import fudge
+from nose.tools import raises
 from jinja2 import Environment
-from virtstrap.templating import environment, temp_template_environment
+from virtstrap.templating import *
 from tests import fixture_path
 
 def test_get_environment():
@@ -22,3 +24,94 @@ def test_test_environment_context():
         env.get_template('tests/test_template.sh.jinja')
     env = environment()
     template = env.get_template('init/activate.sh.jinja')
+
+def test_tempita_environment():
+    env = TempitaEnvironment()
+
+class TestTempitaEnvironment(object):
+    def setup(self):
+        self.env = TempitaEnvironment()
+
+    def setup_loaders(self):
+        fake_loader = fudge.Fake()
+        self.env.add_loader(fake_loader)
+        return fake_loader
+
+    
+    @fudge.test
+    def test_add_loader(self):
+        fake_loader = self.setup_loaders()
+        (fake_loader.expects('get_template').with_args('name', self.env)
+                .returns('sometemplate'))
+        self.env.add_loader(fake_loader)
+        template = self.env.get_template('name')
+        assert template == 'sometemplate'
+    
+    @raises(TemplateDoesNotExist)
+    @fudge.test
+    def test_get_template_fails(self):
+        fake_loader = self.setup_loaders()
+        (fake_loader.expects('get_template').with_args('name2', self.env)
+                .returns(None))
+        self.env.get_template('name2')
+
+def test_template():
+    template = Template('template')
+
+class TestTempitaTemplate(object):
+    def setup(self):
+        fake_tempita_template = fudge.Fake()
+        self.template = Template(fake_tempita_template)
+        self.fake_tempita_template = fake_tempita_template
+    
+    @fudge.test
+    def test_render(self):
+        context = dict(a='hello')
+        (self.fake_tempita_template.expects('substitute')
+            .with_args(context)
+            .returns('somerendering'))
+        assert self.template.render(context) == 'somerendering'
+
+@raises(NotImplementedError)
+def test_loader():
+    loader = TemplateLoader()
+    template = loader.get_template('tests/test_template.tmpl', None)
+    #assert template.render(dict(name="hello")) == "hello"
+
+def fake_environment():
+    fake_env = fudge.Fake('TempitaEnvironment')
+    fake_env.provides('get_template').returns('template')
+    fake_env.provides('get_tempita_template').returns('template')
+    return fake_env
+
+@fudge.patch('virtstrap.templating.Template')
+def test_loader_load_template_file(fake_template_class):
+    loader = TemplateLoader()
+    fake_env = fudge.Fake()
+    fake_env.has_attr(get_template='something')
+    fake_template_class.expects('from_filename').returns('template')
+    template = loader.load_template_file('path', fake_env)
+    assert template == 'template'
+
+def test_file_system_loader():
+    loader = FileSystemLoader(fixture_path('templates'))
+    fake_env = fake_environment()
+    template = loader.get_template('tests/test_template.tmpl', fake_env)
+    assert template is not None
+    
+class TestHighLevelTempitaTemplate(object):
+    def setup(self):
+        template_path = fixture_path('templates')
+        loaders = [FileSystemLoader(template_path)]
+        self.env = TempitaEnvironment(loaders=loaders)
+
+    def test_render_template(self):
+        template = self.env.get_template('tests/test_template.tmpl')
+        rendered = template.render(dict(name="hello")).strip()
+        assert rendered == 'hello'
+
+    def test_render_inheriting_template(self):
+        template = self.env.get_template('tests/test_child.tmpl')
+        rendered = template.render(dict(parent_name="parent", 
+            name="child")).strip()
+        assert rendered == 'parent\nchild'
--- virtstrap-core/virtstrap/templating.py
+++ virtstrap-core/virtstrap/templating.py
@@ -5,10 +5,11 @@ virtstrap.templating
 Defines tools for dealing with templating.
 """
 
+import os
 from contextlib import contextmanager
-from jinja2 import Environment, PackageLoader, FileSystemLoader
-
-__all__ = ['environment', 'temp_template_environment']
+from jinja2 import (Environment, PackageLoader as JinjaPackageLoader, 
+        FileSystemLoader as JinjaFileSystemLoader)
+from packages.tempita import Template as TempitaTemplate
 
 _global_environment = None
 
@@ -17,7 +18,7 @@ def environment():
     global _global_environment
     if not _global_environment:
         _global_environment = Environment(
-                loader=PackageLoader('virtstrap', 'templates'))
+                loader=JinjaPackageLoader('virtstrap', 'templates'))
     return _global_environment
 
 @contextmanager
@@ -32,8 +33,79 @@ def temp_template_environment(directory):
     if _global_environment:
         original_environment = None
     _global_environment = Environment(
-            loader=FileSystemLoader(directory))
+            loader=JinjaFileSystemLoader(directory))
     try:
         yield
     finally:
         _global_environment = original_environment
+
+class TemplateDoesNotExist(Exception):
+    pass
+
+class TempitaEnvironment(object):
+    """A tempita environment similar to jinja's environment
+
+    The loader can be updated on the fly.
+    """
+    def __init__(self, loaders=None):
+        loaders = loaders or []
+        self._loaders = loaders[:]
+
+    def add_loader(self, loader):
+        """Last loader takes most precedence"""
+        self._loaders.insert(0, loader)
+
+    def get_template(self, template_name, from_template=None):
+        for loader in self._loaders:
+            template = loader.get_template(template_name, self)
+            if template:
+                return template
+        raise TemplateDoesNotExist(
+                'Template "%s" does not exist' % template_name)
+
+    def get_tempita_template(self, template_name, from_template=None):
+        template = self.get_template(template_name, from_template)
+        return template._template
+
+class Template(object):
+    """Wraps tempita's template to match the current usage of templates."""
+    @classmethod
+    def from_filename(cls, path, env, **kwargs):
+        kwargs['get_template'] = env.get_tempita_template
+        tempita_template = TempitaTemplate.from_filename(path, **kwargs)
+        return cls(tempita_template, env)
+
+    def __init__(self, template, environment=None):
+        self._template = template
+        self._environment = environment
+
+    def render(self, context):
+        return self._template.substitute(context)
+
+class TemplateLoader(object):
+    def get_template(self, name, env):
+        raise NotImplementedError("get_template not implemented")
+
+    def load_template_file(self, path, env):
+        template = Template.from_filename(path, env)
+        return template
+
+class FileSystemLoader(TemplateLoader):
+    def __init__(self, base_path):
+        self._base_path = os.path.abspath(base_path)
+
+    def get_template(self, name, env):
+        template_path = os.path.join(self._base_path, name)
+        return self.load_template_file(template_path, env)
+
+class PackageLoader(TemplateLoader):
+    def __init__(self, package_name, package_path='templates'):
+        from pkg_resources import DefaultProvider, ResourceManager, \
+                                  get_provider
+        self._provider = get_provider(package_name)
+        self._manager = ResourceManager()
+        self._package_path = package_path
+
+    def get_template(self, name, env):
+        template_path = os.path.join(self._base_path, name)
+        return self.load_template_file(template_path, env)
