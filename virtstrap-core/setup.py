0a2c4b2521b7dd864c0a1770736918242683bf66
Reuven V. Gonzales <reuven@tobetter.us>
2012-04-02 15:39:46 -0700

    Updating to version 0.3.6
    Hopefully will stop these extremely small fixes

--- virtstrap-core/setup.py
+++ virtstrap-core/setup.py
@@ -8,7 +8,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.3.5"
+VERSION = "0.3.6"
 
 # Installation requirements
 REQUIREMENTS = []
--- virtstrap/setup.py
+++ virtstrap/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-VERSION = '0.3.5'
+VERSION = '0.3.6'
 
 LONG_DESCRIPTION = open('README.rst').read()
 
