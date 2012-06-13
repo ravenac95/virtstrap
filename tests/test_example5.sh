50e563475a3c33460d82c597c5ccec61566c9bcb
Reuven V. Gonzales <reuven@tobetter.us>
2012-06-13 14:02:38 -0700

    Fixed comments and labeling

--- tests/test_example5.sh
+++ tests/test_example5.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-# file: tests/test_examples.sh
-# This tests the virtstrap examples
+# file: tests/test_example6.sh
+# This tests example5
     
 . ./setup.sh
 
 TEST_DIR=`pwd`
 EXAMPLE5_DIR=$TEST_DIR/../examples/example5
+
+oneTimeSetUp() {
+    true
+}
+
+oneTimeTearDown() {
+    true
+}
 
 setUp() {
     # Make sure the environment variables to test are not set
@@ -17,7 +25,7 @@ tearDown() {
     $PYTHON_BIN cleandir.py $EXAMPLE5_DIR
 }
 
-testInitExample() {
+testInitExample5() {
     vstrap init
     assertEquals 0 $? 
 }
