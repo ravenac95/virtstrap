3f4332fbadd3fcecd603ab0c5a9c01e0a1cccb30
Reuven V. Gonzales <reuven@virtru.com>
2013-04-09 23:59:12 -0400

    Fixing. Now version 0.3.14

--- virtstrap-local/setup.py
+++ virtstrap-local/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.13'
+VERSION = '0.3.14'
 
 setup(name='virtstrap-local',
     version=VERSION,
--- virtstrap/setup.py
+++ virtstrap/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.13'
+VERSION = '0.3.14'
 
 LONG_DESCRIPTION = open('README.rst').read()
 
